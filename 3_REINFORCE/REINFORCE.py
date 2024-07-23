555c00f3e8409624fcd2cd1001e6480ce39922d3
xucheng95 <xuchengsiyou@163.com>
2024-07-23 22:04:31 +0800

    fix: add REINFORCE

--- 3_REINFORCE/REINFORCE.py
+++ 3_REINFORCE/REINFORCE.py
@@ -1,22 +1,36 @@
-import os
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import random
-from collections import deque
-import gymnasium
-import matplotlib.pyplot as plt
 
 
 class PolicyNetwork(nn.Module):
     def __init__(self, state_size, action_size):
-        super(PolicyNetwork, self).__init__()
+        """
+        初始化函数，用于创建神经网络。
+
+        Args:
+            state_size (int): 状态空间的大小。
+            action_size (int): 动作空间的大小。
+
+        Returns:
+            None
+        """
+        super().__init__()
         self.fc1 = nn.Linear(state_size, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, action_size)
 
     def forward(self, x):
+        """
+        前向传播函数，用于计算动作的概率分布。
+
+        Args:
+            x (torch.Tensor): 输入的Tensor，形状为(batch_size, input_dim)。
+
+        Returns:
+            torch.Tensor: 输出的动作概率分布，形状为(batch_size, num_actions)。
+        """
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = self.fc3(x)
@@ -24,25 +38,74 @@ class PolicyNetwork(nn.Module):
 
 
 class REINFORCEAgent:
-    def __init__(self, state_size, action_size, learning_rate=0.001):
+    def __init__(self, state_size, action_size, gamma=0.99, learning_rate=0.001):
+        """
+        初始化函数，用于设置REINFORCE的参数和组件
+
+        Args:
+            state_size (int): 环境的状态大小（例如，环境中的状态空间的大小）
+            action_size (int): 环境中可采取的动作的数量
+            gamma (float): 折扣因子（在[0, 1]之间），用于计算长期奖励
+            learning_rate (float): 学习率，用于优化器
+
+        Returns:
+            None
+        """
         self.state_size = state_size
         self.action_size = action_size
+        self.gamma = gamma
         self.policy_network = PolicyNetwork(state_size, action_size)
         self.optimizer = optim.Adam(self.policy_network.parameters(), lr=learning_rate)
         self.memory = []
 
-    def act(self, state):
+    def select_action(self, state, sample=False):
+        """
+        根据当前状态选择动作。
+        
+        Args:
+            state (np.ndarray): 当前环境的状态，形状为 (state_size,)。
+            sample (bool, optional): 是否根据概率采样动作。默认为False，表示选择概率最大的动作。
+        
+        Returns:
+            int: 选择的动作编号，范围在 [0, action_size) 之间。
+        
+        """
         state = torch.from_numpy(state).float().unsqueeze(0)
         probs = self.policy_network(state).cpu()
-        action = np.random.choice(self.action_size, p=probs.detach().numpy().squeeze())
+        if not sample:
+            action = probs.argmax(dim=-1).item()
+        else:
+            action = np.random.choice(
+                self.action_size, p=probs.detach().numpy().squeeze()
+            )
         return action
 
     def remember(self, state, action, reward):
+        """
+        将经验存储到经验池中，并在满足条件时开始学习。
+        
+        Args:
+            state (np.ndarray): 当前状态。
+            action (int): 当前动作。
+            reward (float): 获得的奖励。
+        
+        Returns:
+            None
+        """
         self.memory.append((state, action, reward))
 
     def learn(self):
+        """
+        根据存储的经验进行策略网络的训练。
+        
+        Args:
+            无参数。
+        
+        Returns:
+            None 
+        """
         states, actions, rewards = zip(*self.memory)
-        discounts = np.array([0.99**i for i in range(len(rewards))])
+        discounts = np.array([self.gamma**i for i in range(len(rewards))])
         returns = np.array(
             [
                 sum(rewards[i:] * discounts[: len(rewards) - i])
@@ -62,77 +125,32 @@ class REINFORCEAgent:
         self.optimizer.step()
         self.memory = []
 
+    def save(self, path, name):
+        """
+        将当前模型的参数保存到指定路径下。
 
-def train():
-    env = gymnasium.make("CartPole-v1")
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-    agent = REINFORCEAgent(state_size, action_size, learning_rate=0.001)
+        Args:
+            path (str): 保存模型的路径。
+            name (str): 模型的名称，用于生成保存文件的名称。
 
-    n_episodes = 1000
-    max_t = 1000
-    print_interval = 100
-    episode_rewards = []
+        Returns:
+            None
+        """
+        torch.save(
+            self.policy_network.state_dict(), f"{path}/reinforce_model_{name}.pt"
+        )
 
-    if not os.path.exists("checkpoints"):
-        os.makedirs("checkpoints")
+    def load(self, path, name):
+        """
+        从指定路径下加载模型的参数，并更新当前模型。
 
-    for i_episode in range(1, n_episodes + 1):
-        state, _ = env.reset()
-        total_rewards = 0
-        for t in range(max_t):
-            action = agent.act(state)
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            total_rewards += reward
-            agent.remember(state, action, reward)
-            state = next_state
-            if terminated or truncated:
-                break
-
-        agent.learn()
-        episode_rewards.append(total_rewards)
-        if i_episode % print_interval == 0:
-            print(f"Episode {i_episode}/{n_episodes} - Score: {total_rewards}")
-
-            torch.save(
-                agent.policy_network.state_dict(),
-                f"checkpoints/reinforce_model_{i_episode}.pt",
-            )
-
-    env.close()
-    plt.plot(episode_rewards)
-    plt.xlabel("Episode")
-    plt.ylabel("Total Reward")
-    plt.title("Reward over Episodes")
-    plt.savefig("reinforce_rewards.png")
-
-
-def test():
-    env = gymnasium.make("CartPole-v1", render_mode="human")
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-    agent = REINFORCEAgent(state_size, action_size, learning_rate=0.001)
-
-    last_episode = 1000
-    agent.policy_network.load_state_dict(
-        torch.load(f"checkpoints/reinforce_model_{last_episode}.pt")
-    )
-
-    for _ in range(5):
-        state, _ = env.reset()
-        total_rewards = 0
-        while True:
-            env.render()
-            action = agent.act(state)
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            total_rewards += reward
-            if terminated or truncated:
-                break
-            state = next_state
-        print(f"Test Completed! Total Rewards: {total_rewards}")
-    env.close()
-
-
-if __name__ == "__main__":
-    train()
-    test()
+        Args:
+            path (str): 保存模型的路径。
+            name (str): 模型的名称，用于生成保存文件的名称。
+        
+        Returns:
+            None
+        """
+        self.policy_network.load_state_dict(
+            torch.load(f"{path}/reinforce_model_{name}.pt")
+        )
---
+++ 3_REINFORCE/main.py
@@ -0,0 +1,125 @@
+import os
+import argparse
+import gymnasium
+import matplotlib.pyplot as plt
+from REINFORCE import REINFORCEAgent
+
+
+def train(args):
+    """
+    训练并在环境中执行以获取奖励。
+
+    Args:
+        args (argparse.Namespace): 包含所有命令行参数的命名空间对象。
+
+    Returns:
+        None
+    """
+    env = gymnasium.make(args.env_name)
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = REINFORCEAgent(
+        state_size,
+        action_size,
+        learning_rate=args.learning_rate,
+    )
+
+    episode_rewards = []
+    if not os.path.exists("checkpoints"):
+        os.makedirs("checkpoints")
+
+    for i_episode in range(1, args.n_episodes + 1):
+        state, _ = env.reset()
+        total_rewards = 0
+        for _ in range(args.max_t):
+            action = agent.select_action(state, sample=True)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            total_rewards += reward
+            done = terminated or truncated
+            agent.remember(state, action, reward)
+            state = next_state
+            if done:
+                break
+        agent.learn()
+        episode_rewards.append(total_rewards)
+        if i_episode % args.print_interval == 0:
+            print(f"Episode {i_episode}/{args.n_episodes} - Score: {total_rewards}")
+        if i_episode % args.checkpoint_interval == 0:
+            agent.save("checkpoints", i_episode)
+    env.close()
+
+    plt.plot(episode_rewards)
+    plt.xlabel("Episode")
+    plt.ylabel("Total Reward")
+    plt.title("Reward over Episodes")
+    plt.savefig("checkpoints/rewards.png")
+
+
+def test(args):
+    """
+    对训练好的agent进行5次测试，并打印每次的总奖励。
+
+    Args:
+        args: argparse.Namespace类型的对象，包含以下属性：
+            - env_name (str): 环境名称。
+            - render_mode (str): 渲染模式，可选值为'human'或'rgb_array'。
+            - n_episodes (int): 从检查点中加载的代理模型的序号。
+
+    Returns:
+        None
+    """
+    env = gymnasium.make(args.env_name, render_mode=args.render_mode)
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = REINFORCEAgent(state_size, action_size)
+    agent.load("checkpoints", args.n_episodes)
+
+    for _ in range(5):
+        state, _ = env.reset()
+        total_rewards = 0
+        while True:
+            env.render()
+            action = agent.select_action(state)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            total_rewards += reward
+            if terminated or truncated:
+                break
+            state = next_state
+        print(f"Test Completed! Total Rewards: {total_rewards}")
+    env.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--test_mode", action="store_true", help="是否为测试模式，默认为False"
+    )
+    parser.add_argument(
+        "--env_name", default="CartPole-v1", help="环境名称，默认为CartPole-v1"
+    )
+    parser.add_argument("--render_mode", default="human", help="渲染模式，默认为human")
+    parser.add_argument(
+        "--gamma", default=0.99, type=float, help="折扣因子，默认为0.99"
+    )
+    parser.add_argument(
+        "--learning_rate", default=0.001, type=float, help="学习率，默认为0.001"
+    )
+    parser.add_argument(
+        "--n_episodes", default=1000, type=int, help="训练回合数，默认为1000"
+    )
+    parser.add_argument(
+        "--max_t", default=2000, type=int, help="最大时间步数，默认为2000"
+    )
+    parser.add_argument(
+        "--print_interval", default=10, type=int, help="打印间隔，默认为10"
+    )
+    parser.add_argument(
+        "--checkpoint_interval", default=100, type=int, help="检查点间隔，默认100"
+    )
+    args = parser.parse_args()
+
+    if args.test_mode:
+        test(args)
+    else:
+        train(args)
+        test(args)
